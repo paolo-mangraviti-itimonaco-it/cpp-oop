f9e0d82bea062942d61b4841f1add553d09f7876
Paolo Mangraviti <80854427+paolo-mangraviti-itimonaco-it@users.noreply.github.com>
2025-10-08 05:48:37 +0200

    Update 001-puntatori.cpp

--- 001-puntatori.cpp
+++ 001-puntatori.cpp
@@ -3,19 +3,6 @@
 
 using namespace std;
 
-// Metodo 'incrementa' con il legame per riferimento (o referenza) tra parametro formale e parametro attuale 
-// parametri formali: intero,intero
-void incrementa(int &v,int passo)
-{
-    v += passo;
-}
-
-// parametri formali: puntatore a intero,int
-void incrementa(int* v,int passo)
-{
-    cout << "funzione 2" << endl;
-    *v += passo;
-}
 
 int main()
 {
@@ -30,47 +17,21 @@ int main()
     cout << endl;
     cout << "Valore della variabile (deferenziazione-referenzazione): " << *(&a);
     cout << endl << endl;
-    int* pa = &a;
-    cout << "Valore del puntatore pa: " << pa;
+    int* pa1 = &a;
+    //int& pa2 = a;            // equivalente al precedente
+    cout << "Valore del puntatore pa: " << pa1;
     cout << endl;
-    cout << "Valore puntato da pa: " << *pa;
+    cout << "Valore puntato da pa: " << *pa1;
 
     cout << endl << endl;
     // Incremento del valore della variabile 'a'
-    (*pa)++;
+    (*pa1)++;
     cout << "Valore della variabile: " << a;
     cout << endl;
     cout << "Puntatore (indirizzo) della variabile: " << &a;
 
     cout << endl << endl << endl;
-    cout << "Puntatori di tipi strutturati";
-    cout << endl << endl;
-    int b[4] = {2,8,1,0};
-    cout << endl;
-    cout << "array b: "  << b;
-    cout << endl << endl;
-    cout << "inidirizzo array b: "   << &b;
-    cout << endl << endl;
-    cout << "array b  (elementi) -  " << b[0] << " - " << b[1] << " - " << b[2] << " - " << b[3];
-    cout << endl << endl;
-    cout << "array b (elementi) Punt:valore -  " << &b[0] << ":" << b[0] << " - " << &b[1] << ":" << b[1]<< " - " << &b[2] << ":" << b[2] << " - " << &b[3] << ":" << b[3];
-    cout << endl << endl;
-    int* pb[4] = {&b[3],&b[2],&a,pa};
-    cout << "array pb  -  " << pb[0] << " - " << pb[1] << " - " << pb[2] << " - " << pb[3];
-    cout << endl << endl;
-    cout<< "array pb  Punt:valore -  " << pb[0] << ":" << *pb[0] << " - " << pb[1] << ":" << *pb[1]<< " - " << pb[2] << ":" << *pb[2] << " - " << pb[3] << ":" << *pb[3];
-    cout << endl << endl;
-    (*pb[2])++;
-    cout<< "array pb  Punt:valore -  " << pb[0] << ":" << *pb[0] << " - " << pb[1] << ":" << *pb[1]<< " - " << pb[2] << ":" << *pb[2] << " - " << pb[3] << ":" << *pb[3];
-    cout << endl << endl;
-    cout << "Valore della variabile 'a': " << a;
-    cout << endl << endl;
-    incrementa(a,3);
-    cout << "Valore della variabile 'a': " << a;
-    cout << endl << endl;
-    incrementa(pb[0],5);
-    cout << "Valore della variabile 'pb[0]' e del valore puntato: " << pb[0] << ":" << *pb[0];
-    cout << endl << endl << endl << endl;
+    
 
     return 0;
 }
